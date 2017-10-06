05e8a53d04b99de089b1a83db5f4d96c830802b6
kvanee <kelly.vanee@gmail.com>
2017-10-05 21:58:39 -0700

    multiplayer working (with jank)

--- app.js
+++ app.js
@@ -30,17 +30,100 @@ var Player = function (startX, startY, startAngle) {
 }
 
 //onNewplayer function is called whenever a server gets a message “new_player” from the client
+// when a new player connects, we make a new instance of the player object,
+// and send a new player message to the client. 
 function onNewplayer (data) {
-	//form a new player object 
+	console.log(data);
+	//new player instance
 	var newPlayer = new Player(data.x, data.y, data.angle);
+	console.log(newPlayer);
 	console.log("created new player with id " + this.id);
+	newPlayer.id = this.id; 	
+	//information to be sent to all clients except sender
+	var current_info = {
+		id: newPlayer.id, 
+		x: newPlayer.x,
+		y: newPlayer.y,
+		angle: newPlayer.angle,
+	}; 
+	
+	//send to the new player about everyone who is already connected. 	
+	for (i = 0; i < player_lst.length; i++) {
+		existingPlayer = player_lst[i];
+		var player_info = {
+			id: existingPlayer.id,
+			x: existingPlayer.x,
+			y: existingPlayer.y, 
+			angle: existingPlayer.angle,			
+		};
+		console.log("pushing player");
+		//send message to the sender-client only
+		this.emit("new_enemyPlayer", player_info);
+	}
+	
+	//send message to every connected client except the sender
+	this.broadcast.emit('new_enemyPlayer', current_info);
 	player_lst.push(newPlayer); 
-
 }
+
+//update the player position and send the information back to every client except sender
+function onMovePlayer (data) {
+	
+	var movePlayer = find_playerid(this.id); 
+	movePlayer.x = data.x;
+	movePlayer.y = data.y;
+	movePlayer.angle = data.angle; 
+	
+	var moveplayerData = {
+		id: movePlayer.id,
+		x: movePlayer.x,
+		y: movePlayer.y, 
+		angle: movePlayer.angle
+	}
+	console.log('move player:' + JSON.stringify(moveplayerData)); 
+	//send message to every connected client except the sender
+	this.broadcast.emit('enemy_move', moveplayerData);
+}
+
+//call when a client disconnects and tell the clients except sender to remove the disconnected player
+function onClientdisconnect() {
+	console.log('disconnect'); 
+
+	var removePlayer = find_playerid(this.id); 
+		
+	if (removePlayer) {
+		player_lst.splice(player_lst.indexOf(removePlayer), 1);
+	}
+	
+	console.log("removing player " + this.id);
+	
+	//send message to every connected client except the sender
+	this.broadcast.emit('remove_player', {id: this.id});
+	
+}
+
+// find player by the the unique socket id 
+function find_playerid(id) {
+	
+		for (var i = 0; i < player_lst.length; i++) {
+	
+			if (player_lst[i].id == id) {
+				return player_lst[i]; 
+			}
+		}
+		
+		return false; 
+	}
 
 // listen for a connection request from any client
 io.sockets.on('connection', function(socket){
 	console.log("socket connected"); 
 	//output a unique socket.id 
-	console.log(socket.id);
+	// listen for disconnection; 
+	socket.on('disconnect', onClientdisconnect); 
+	
+	// listen for new player
+	socket.on("new_player", onNewplayer);
+	// listen for player position update
+	socket.on("move_player", onMovePlayer);
 });
--- public/js/client.js
+++ public/js/client.js
@@ -3,15 +3,22 @@ socket = io.connect();
 
 canvas_width = window.innerWidth * window.devicePixelRatio; 
 canvas_height = window.innerHeight * window.devicePixelRatio;
-var game = new Phaser.Game(canvas_width, canvas_height, Phaser.CANVAS, 'phaser-example');
+var game = new Phaser.Game(canvas_width, canvas_height, Phaser.CANVAS, 'gameDiv');
+
+var enemies = {};
 
 var gameProperties = { 
 	//this is the actual game size to determine the boundary of 
 	//the world
 	gameWidth: 4000, 
 	gameHeight: 4000,
+	game_elemnt: "gameDiv",
+	in_game: false,
 };
 
+var playerProperties = {
+	speed: 400
+};
 // this is the main game state
 var main = function(game){
 };
@@ -24,6 +31,18 @@ function onsocketConnected () {
 	// send to the server a "new_player" message so that the server knows
 	// a new player object has been created
 	socket.emit('new_player', {x: 0, y: 0, angle: 0});
+}
+
+function onRemovePlayer (data) {
+	var removePlayer = findplayerbyid(data.id);
+	// Player not found
+	if (!removePlayer) {
+		console.log('Player not found: ', data.id)
+		return;
+	}
+	
+	removePlayer.player.destroy();
+	enemies.splice(enemies.indexOf(removePlayer), 1);
 }
 
 function createPlayer () {
@@ -41,16 +60,68 @@ function createPlayer () {
 	player.body_size = player.radius; 
 
 	// draw a shape
-	game.physics.p2.enableBody(player, true);
-	player.body.addCircle(player.body_size, 0 , 0); 
+	game.physics.enable(player, Phaser.Physics.ARCADE);
+}
+
+// this is the enemy class. 
+var remote_player = function (id, startx, starty, start_angle) {
+	this.x = startx;
+	this.y = starty;
+	//this is the unique socket id. We use it as a unique name for enemy
+	this.id = id;
+	this.angle = start_angle;
+	
+	this.player = game.add.graphics(this.x , this.y);
+	this.player.radius = 100;
+
+	// set a fill and line style
+	this.player.beginFill(0xffd900);
+	this.player.lineStyle(2, 0xffd900, 1);
+	this.player.drawCircle(0, 0, this.player.radius * 2);
+	this.player.endFill();
+	this.player.anchor.setTo(0.5,0.5);
+	this.player.body_size = this.player.radius; 
+
+	// draw a shape
+	game.physics.enable(this.player, Phaser.Physics.ARCADE);
+	//this.player.body.clearShapes();
+	//this.player.body.addCircle(this.player.body_size, 0 , 0); 
+	//this.player.body.data.shapes[0].sensor = true;
+}
+
+//Server will tell us when a new enemy player connects to the server.
+//We create a new enemy in our game.
+function onNewPlayer (data) {
+	console.log("new player");
+	console.log(data);
+	//enemy object 
+	var new_enemy = new remote_player(data.id, data.x, data.y, data.angle); 
+	enemies[data.id] = new_enemy;
+}
+
+//Server tells us there is a new enemy movement. We find the moved enemy
+//and sync the enemy movement with the server
+function onEnemyMove (data) {
+	var movePlayer = enemies[data.id]; 
+	
+	if (!movePlayer) {
+		return;
+	}
+	movePlayer.x = data.x; 
+	movePlayer.y = data.y; 
+	movePlayer.angle = data.angle; 
+	var distance = Phaser.Math.distance(movePlayer.player.x,movePlayer.player.y,data.x, data.y);
+	console.log("distance: " + distance);
+	if(distance > 10)
+		game.physics.arcade.moveToXY(movePlayer.player, movePlayer.x, movePlayer.y, playerProperties.speed);
 }
 
 // add the 
 main.prototype = {
 	preload: function() {
+		game.stage.disableVisibilityChange = true;
 		game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-		game.world.setBounds(0, 0, gameProperties.gameWidth, 
-		gameProperties.gameHeight, false, false, false, false);
+		game.world.setBounds(0, 0, gameProperties.gameWidth, gameProperties.gameHeight, false, false, false, false);
 		//I’m using P2JS for physics system. You can choose others if you want
 		game.physics.startSystem(Phaser.Physics.P2JS);
 		game.physics.p2.setBoundsToWorld(false, false, false, false, false)
@@ -60,7 +131,7 @@ main.prototype = {
 		game.physics.p2.applyGravity = false; 
 		game.physics.p2.enableBody(game.physics.p2.walls, false); 
 		// turn on collision detection
-		game.physics.p2.setImpactEvents(true);
+		//game.physics.p2.setImpactEvents(true);
 
     },
 	//this function is fired once when we load the game
@@ -72,23 +143,32 @@ main.prototype = {
 		//the client connects, call onsocketConnected.  
 		onsocketConnected();
 
+		//listen to new enemy connections
+		socket.on("new_enemyPlayer", onNewPlayer);
+		//listen to enemy movement 
+		socket.on("enemy_move", onEnemyMove);
+		
+		// when received remove_player, remove the player passed; 
+		socket.on('remove_player', onRemovePlayer);
     },
     update: function () {
-		if (gameProperties.in_game)
+		if (gameProperties.in_game && game.input.mousePointer.isDown)
 		{
-			//  400 is the speed it will move towards the mouse
-			game.physics.arcade.moveToPointer(sprite, 400);
-	
+			var pointer = game.input.activePointer;
+
 			//  if it's overlapping the mouse, don't move any more
 			if (game.physics.arcade.distanceToPointer(player, pointer) <= 50)
-			{
-				sprite.body.velocity.setTo(0, 0);
-			}
+				player.body.velocity.setTo(0, 0);
+			
+			else
+				game.physics.arcade.moveToPointer(player, playerProperties.speed);
+			
 		}
 		else
 		{
-			sprite.body.velocity.setTo(0, 0);
+			player.body.velocity.setTo(0, 0);
 		}
+		socket.emit('move_player', {x: player.x, y: player.y, angle: player.angle});
 	}
 }
 
